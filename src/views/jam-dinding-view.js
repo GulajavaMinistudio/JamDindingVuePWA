8f91fb3846c2c440e45f57c7bcbcf03e7de0a82e
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-02-04 08:03:01 +0700

    Perbaikan kode

--- src/views/jam-dinding-view.js
+++ src/views/jam-dinding-view.js
@@ -1,6 +1,5 @@
 import { mapGetters } from 'vuex';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-// import '@fortawesome/fontawesome-free/js/all.min';
 
 const JamDindingComponent = () => import(/* webpackChunkName: "jam-dinding-component" */ '@/components/jam-dinding/JamDindingComponent.vue');
 
